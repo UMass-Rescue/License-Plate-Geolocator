23c5cb685ee8bd2d829d648195caf0e6cf42c462
Akhilareddyd <d.akhila.reddy33@gmail.com>
2024-03-10 20:48:41 -0400

    detect.py classes

--- Backend /Model.py
+++ Backend /Model.py
@@ -1,9 +1,4 @@
 import tensorflow as tf
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-from absl import app, flags, logging
-from absl.flags import FLAGS
 import core.utils as utils
 from core.config import cfg
 from core.yolov4 import filter_boxes
@@ -14,96 +9,93 @@ import numpy as np
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
-flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
-flags.DEFINE_string('weights', './checkpoints/yolov4-416',
-                    'path to weights file')
-flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
-flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
-flags.DEFINE_list('images', './data/images/kite.jpg', 'path to input image')
-flags.DEFINE_string('output', './detections/', 'path to output folder')
-flags.DEFINE_float('iou', 0.45, 'iou threshold')
-flags.DEFINE_float('score', 0.25, 'score threshold')
-flags.DEFINE_boolean('dont_show', False, 'dont show image output')
+class LicensePlateDetector:
+    def __init__(self):
+        self.framework = 'tf'
+        self.weights_path = './checkpoints/yolov4-416'
+        self.size = 416
+        self.tiny = False
+        self.model = 'yolov4'
+        self.images_path = ['./data/images/kite.jpg']
+        self.output_path = './detections/'
+        self.iou_threshold = 0.45
+        self.score_threshold = 0.25
+        self.dont_show = False
 
-def main(_argv):
-    config = ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
-    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
-    input_size = FLAGS.size
-    images = FLAGS.images
+    def detect_license_plates(self):
+        # Initialize TensorFlow session
+        physical_devices = tf.config.experimental.list_physical_devices('GPU')
+        if len(physical_devices) > 0:
+            tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        config = ConfigProto()
+        config.gpu_options.allow_growth = True
+        session = InteractiveSession(config=config)
 
-    # load model
-    if FLAGS.framework == 'tflite':
-            interpreter = tf.lite.Interpreter(model_path=FLAGS.weights)
-    else:
-            saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+        # Load YOLO config
+        STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config({
+            'size': self.size,
+            'tiny': self.tiny,
+            'model': self.model
+        })
 
-    # loop through images in list and run Yolov4 model on each
-    for count, image_path in enumerate(images, 1):
-        original_image = cv2.imread(image_path)
-        original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
-
-        image_data = cv2.resize(original_image, (input_size, input_size))
-        image_data = image_data / 255.
-
-        images_data = []
-        for i in range(1):
-            images_data.append(image_data)
-        images_data = np.asarray(images_data).astype(np.float32)
-
-        if FLAGS.framework == 'tflite':
-            interpreter.allocate_tensors()
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-            print(input_details)
-            print(output_details)
-            interpreter.set_tensor(input_details[0]['index'], images_data)
-            interpreter.invoke()
-            pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
-            if FLAGS.model == 'yolov3' and FLAGS.tiny == True:
-                boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
-            else:
-                boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=0.25, input_shape=tf.constant([input_size, input_size]))
+        # Load model
+        if self.framework == 'tflite':
+            interpreter = tf.lite.Interpreter(model_path=self.weights_path)
         else:
-            infer = saved_model_loaded.signatures['serving_default']
-            batch_data = tf.constant(images_data)
-            pred_bbox = infer(batch_data)
-            for key, value in pred_bbox.items():
-                boxes = value[:, :, 0:4]
-                pred_conf = value[:, :, 4:]
+            saved_model_loaded = tf.saved_model.load(self.weights_path, tags=[tag_constants.SERVING])
 
-        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
-            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
-            scores=tf.reshape(
-                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
-            max_output_size_per_class=50,
-            max_total_size=50,
-            iou_threshold=FLAGS.iou,
-            score_threshold=FLAGS.score
-        )
-        pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+        # Loop through images and run YOLOv4 model on each
+        for count, image_path in enumerate(self.images_path, 1):
+            original_image = cv2.imread(image_path)
+            original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
 
-        # read in all class names from config
-        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+            image_data = cv2.resize(original_image, (self.size, self.size))
+            image_data = image_data / 255.
 
-        # by default allow all classes in .names file
-        allowed_classes = list(class_names.values())
-        
-        # custom allowed classes (uncomment line below to allow detections for only people)
-        #allowed_classes = ['person']
+            images_data = np.expand_dims(image_data, axis=0).astype(np.float32)
 
-        image = utils.draw_bbox(original_image, pred_bbox, allowed_classes = allowed_classes)
+            if self.framework == 'tflite':
+                interpreter.allocate_tensors()
+                input_details = interpreter.get_input_details()
+                output_details = interpreter.get_output_details()
+                interpreter.set_tensor(input_details[0]['index'], images_data)
+                interpreter.invoke()
+                pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+                if self.model == 'yolov3' and self.tiny:
+                    boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=self.score_threshold,
+                                                    input_shape=tf.constant([self.size, self.size]))
+                else:
+                    boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=self.score_threshold,
+                                                    input_shape=tf.constant([self.size, self.size]))
+            else:
+                infer = saved_model_loaded.signatures['serving_default']
+                batch_data = tf.constant(images_data)
+                pred_bbox = infer(batch_data)
+                for key, value in pred_bbox.items():
+                    boxes = value[:, :, 0:4]
+                    pred_conf = value[:, :, 4:]
 
-        image = Image.fromarray(image.astype(np.uint8))
-        if not FLAGS.dont_show:
-            image.show()
-        image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
-        cv2.imwrite(FLAGS.output + 'detection' + str(count) + '.png', image)
+            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+                scores=tf.reshape(
+                    pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+                max_output_size_per_class=50,
+                max_total_size=50,
+                iou_threshold=self.iou_threshold,
+                score_threshold=self.score_threshold
+            )
+            pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
 
-if __name__ == '__main__':
-    try:
-        app.run(main)
-    except SystemExit:
-        pass
+            # Read class names from config
+            class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+            # By default allow all classes in .names file
+            allowed_classes = list(class_names.values())
+
+            image = utils.draw_bbox(original_image, pred_bbox, allowed_classes=allowed_classes)
+
+            image = Image.fromarray(image.astype(np.uint8))
+            if not self.dont_show:
+                image.show()
+            image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+            cv2.imwrite(self.output_path + 'detection' + str(count) + '.png', image)
--- Backend /app.py
+++ Backend /app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from werkzeug.utils import secure_filename
+import os
+from clpr import get_license_plates
 
-# Create an instance of Flask
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super_secretive_key'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
-# Define a route for the home page
-@app.route('/')
-def index():
-    return 'Hello, World! This is the home page.'
+class UploadFileForm(FlaskForm):
+    file = FileField("File")
+    submit = SubmitField("Get Plates")
 
-# Define a route for other pages
-@app.route('/about')
-def about():
-    return 'About page'
+@app.route('/', methods=["GET", "POST"])
+@app.route('/home', methods=["GET", "POST"])
+def home():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+        
+    return render_template('index.html', form=form)
 
-# Run the application
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run(debug=True)
